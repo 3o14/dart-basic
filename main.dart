a83beeaf39317422fc64e2340a1fa66edbe49436
3o14 <dldnjswn4265@gmail.com>
2023-02-13 23:34:50 +0900

    dart basic

---
+++ main.dart
@@ -0,0 +1,40 @@
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  // constructor 생성자
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var apiData = [
+    {
+      "name": "wonju",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "mirei",
+      "team": "grey",
+      "xp": 10,
+    },
+    {
+      "name": "3o14",
+      "team": "purple",
+      "xp": 110,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
+}
